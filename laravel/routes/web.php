2061919eb6ba68a65fb8c888a178193ee7d97a5e
Dava Ihza Bagus S <dava.ihza18@gmail.com>
2025-04-08 22:18:45 +0700

    Merubah routing web.php
    Mendefinisikan rute untuk menampilkan form dan menyimpan artikel

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArtikelController;
 
 Route::get('/', function () {
     return view('index');
 })->name('index');
 
+Route::get('/artikel/create', [ArtikelController::class, 'create'])->name('artikel.create');
+
+Route::post('/artikel', [ArtikelController::class, 'store'])->name('artikel.store');
