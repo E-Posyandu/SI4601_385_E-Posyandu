248883689370ad7a1c6f205abaac2fdc797a0394
Dava Ihza Bagus S <dava.ihza18@gmail.com>
2025-04-08 20:16:34 +0700

    Membuat ArtikelController.php
    Berfungsi untuk menampilkan form di mana admin bisa memasukkan judul, isi, penulis, dan visibilitas artikel

---
+++ laravel/app/Http/Controllers/ArtikelController.php
@@ -0,0 +1,11 @@
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ArtikelController extends Controller
+{
+    public function create()
+    {
+        return view('admin-side.artikel.create');
+    }
+}
