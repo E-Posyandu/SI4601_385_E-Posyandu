d751e486cf762df619aa28e03a56d091d93e68cc
depislbn <deviherminasilaban@gmail.com>
2025-04-08 17:42:27 +0700

    Update dashboard & routes, add LoginController

---
+++ laravel/resources/views/admin-side/layout-admin/dashboard.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dashboard</title>
+</head>
+<body>
+    <h1>Selamat Datang di Dashboard</h1>
+    <p>Anda berhasil login sebagai <strong>{{ session('admin')->username }}</strong>.</p>
+</body>
+</html>
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return view('index');
-})->name('index');
 
+
+Route::get('/', [AuthController::class, 'loginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/dashboard', [AuthController::class, 'dashboard']);
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
