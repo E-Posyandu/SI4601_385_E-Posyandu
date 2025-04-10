79c4983d6286bf6192d9960064c69444b4aa91fc
Salwafadiaa <salwa.ockry@gmail.com>
2025-04-10 20:18:07 +0700

    PPL385-24 update web

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,13 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CreateJadwalKegiatanController;
 
-// Lihat halaman list jadwal
-Route::get('/admin/jadwal-kegiatan', [CreateJadwalKegiatanController::class, 'index'])->name('jadwal-kegiatan.index');
+// Halaman daftar jadwal
+Route::get('/admin/jadwal-kegiatan', [CreateJadwalKegiatanController::class, 'index'])
+    ->name('jadwal-kegiatan.index');
 
 // Simpan jadwal baru
-Route::post('/admin/jadwal-kegiatan/store', [CreateJadwalKegiatanController::class, 'store'])->name('jadwal-kegiatan.store');
+Route::post('/admin/jadwal-kegiatan/store', [CreateJadwalKegiatanController::class, 'store'])
+    ->name('jadwal-kegiatan.store');
 
 // Update jadwal
-Route::put('/admin/jadwal-kegiatan/update/{id}', [CreateJadwalKegiatanController::class, 'update'])->name('jadwal-kegiatan.update');
+Route::put('/admin/jadwal-kegiatan/update/{id}', [CreateJadwalKegiatanController::class, 'update'])
+    ->name('jadwal-kegiatan.update');
 
 // Hapus jadwal
-Route::delete('/admin/jadwal-kegiatan/delete/{id}', [CreateJadwalKegiatanController::class, 'destroy'])->name('jadwal-kegiatan.destroy');
+Route::delete('/admin/jadwal-kegiatan/delete/{id}', [CreateJadwalKegiatanController::class, 'destroy'])
+    ->name('jadwal-kegiatan.destroy');
