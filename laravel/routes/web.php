834f811d0301f8cef281b1f8eb40f7f564fd296b
Salwafadiaa <salwa.ockry@gmail.com>
2025-04-19 19:08:08 +0700

    PPL385-24 crud jadwal

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -3,9 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JadwalKegiatanController;
 
-// Redirect halaman utama ke jadwal-kegiatan
 Route::get('/', function () {
-    return redirect()->route('jadwal-kegiatan.index');
-});
-
-Route::resource('jadwal-kegiatan', JadwalKegiatanController::class);
+    return view('index');
+})->name('index'); 
